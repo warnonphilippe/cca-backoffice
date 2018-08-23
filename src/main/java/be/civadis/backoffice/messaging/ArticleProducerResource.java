cc770642458c86606dcd41c45f92b93b6fdc3422
Philippe Warnon <phw@civadis.be>
2018-08-23 13:02:46 +0200

    publish / subscribe article - correction key

--- src/main/java/be/civadis/backoffice/messaging/ArticleProducerResource.java
+++ src/main/java/be/civadis/backoffice/messaging/ArticleProducerResource.java
@@ -2,17 +2,15 @@ package be.civadis.backoffice.messaging;
 
 import be.civadis.backoffice.domain.Article;
 import com.codahale.metrics.annotation.Timed;
-import org.mapstruct.ap.internal.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -26,17 +24,18 @@ public class ArticleProducerResource{
     @Timed
     public void produce() {
 
-        List<Article> articleList = new ArrayList<>();
-        articleList.add(new Article(1L, "111", "Article 111"));
-        articleList.add(new Article(2L, "222", "Article 222"));
-        articleList.add(new Article(3L, "333", "Article 333"));
+        List<Article> articleList = Arrays.asList(
+            new Article(1L, "111", "Article 111"),
+            new Article(2L, "222", "Article 222"),
+            new Article(3L, "333", "Article 333")
+        ) ;
 
-        articleList.stream().forEach(art -> {
+        articleList.stream().forEach(art ->
             articleChannel.messageChannel().send(MessageBuilder
                 .withPayload(art)
                 .setHeader(KafkaHeaders.MESSAGE_KEY, art.getId().toString().getBytes(StandardCharsets.UTF_8))
-                .build());
-        });
+                .build())
+        );
 
     }
 
